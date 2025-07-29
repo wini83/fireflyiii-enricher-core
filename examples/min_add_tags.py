04b3d1f445840aa8fed9c043bcc47d9d8e3543b8
wini83 <mariusz.wincior@gmail.com>
2025-07-29 18:12:11 +0200

    fix(SimplifiedTx): notes added

--- examples/min_add_tags.py
+++ examples/min_add_tags.py
@@ -14,12 +14,13 @@ FIREFLY_URL = os.getenv("FIREFLY_URL")
 FIREFLY_TOKEN = os.getenv("FIREFLY_TOKEN")
 
 if FIREFLY_URL is None or FIREFLY_TOKEN is None:
-    raise RuntimeError("Missing FIREFLY_URL or FIREFLY_TOKEN in environment.")
+    raise RuntimeError("Missing FIREFLY_URL or FIREFLY_TOKEN in environment. ")
 
 # Initialize Firefly III client with credentials
 firefly = FireflyClient(base_url=FIREFLY_URL, token=FIREFLY_TOKEN)
 
 TX_ID = 3896
 response = firefly.add_tag_to_transaction(TX_ID, "test tag#3")
+
 
 print(json.dumps(response, indent=2, ensure_ascii=False))
